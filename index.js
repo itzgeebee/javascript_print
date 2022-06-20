0306a08d79716ff2b4ecaa3ccdd9b2cb9b3e7e93
Gideon <balogungideon12@gmail.com>
2022-06-20 17:34:18 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,6 @@
+const fName = "Gideon Balogun"
+const height = "6ft9"
+const country = "Benin Repulic"
+
+document.write(`My name is ${fName}, I am ${height} tall and my country is ${country}`)
+console.log(`My name is ${fName}, I am ${height} tall and my country is ${country}`)
